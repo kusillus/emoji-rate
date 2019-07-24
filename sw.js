98595fa201947caa08c51b9a79d580be3fafff37
Wilder Alexander Guevara Benites <wguevarab7@gmail.com>
2019-07-23 20:40:10 -0500

    Updates

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
-importScripts('/holocard/_nuxt/workbox.4c4f5ca6.js')
+importScripts('/emoji-rate/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/holocard/_nuxt/1512e014730eafd009a0.js",
+    "url": "/emoji-rate/_nuxt/1512e014730eafd009a0.js",
     "revision": "d36edbcccc77e2106e51b5955740ddb2"
   },
   {
-    "url": "/holocard/_nuxt/50a3d230e415e443a15e.js",
-    "revision": "d7db3e108488e656e4c9baff539480dd"
+    "url": "/emoji-rate/_nuxt/d38a05cc4c012589862a.js",
+    "revision": "94ab9ed19c81d9ba799cadf81cdcb5a5"
   },
   {
-    "url": "/holocard/_nuxt/bb57492dba80c1ee0ec7.js",
-    "revision": "ea20caab9b93b5460a8b7cd753aa5b49"
-  },
-  {
-    "url": "/holocard/_nuxt/ef182102c5a7e256495d.js",
+    "url": "/emoji-rate/_nuxt/ef182102c5a7e256495d.js",
     "revision": "44608b82c725e8c031e68d6b8b2eddeb"
+  },
+  {
+    "url": "/emoji-rate/_nuxt/fcd968a3cfaea2ccc1ad.js",
+    "revision": "791240d5af463aa813d32f350ea7bb23"
   }
 ], {
   "cacheId": "emoji-rate",
@@ -26,6 +26,6 @@ workbox.precaching.precacheAndRoute([
 workbox.clientsClaim()
 workbox.skipWaiting()
 
-workbox.routing.registerRoute(new RegExp('/holocard/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/emoji-rate/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
-workbox.routing.registerRoute(new RegExp('/holocard/.*'), workbox.strategies.networkFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/emoji-rate/.*'), workbox.strategies.networkFirst({}), 'GET')
