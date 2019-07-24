c97e39986cb38c0e6f96dd2db109dd75805eeda1
Wilder Alexander Guevara Benites <wguevarab7@gmail.com>
2019-07-23 20:38:15 -0500

    Updates

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-importScripts('/_nuxt/workbox.4c4f5ca6.js')
+importScripts('/holocard/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/1512e014730eafd009a0.js",
+    "url": "/holocard/_nuxt/1512e014730eafd009a0.js",
     "revision": "d36edbcccc77e2106e51b5955740ddb2"
   },
   {
-    "url": "/_nuxt/3ab3bbee31e3b5734dda.js",
-    "revision": "e37555326d8a30f679f15e3fba9a9e30"
+    "url": "/holocard/_nuxt/50a3d230e415e443a15e.js",
+    "revision": "d7db3e108488e656e4c9baff539480dd"
   },
   {
-    "url": "/_nuxt/65dcc7a45ad9a8fa3cd2.js",
-    "revision": "ab6fb75b9e49e2678d72953666c96698"
+    "url": "/holocard/_nuxt/bb57492dba80c1ee0ec7.js",
+    "revision": "ea20caab9b93b5460a8b7cd753aa5b49"
   },
   {
-    "url": "/_nuxt/ef182102c5a7e256495d.js",
+    "url": "/holocard/_nuxt/ef182102c5a7e256495d.js",
     "revision": "44608b82c725e8c031e68d6b8b2eddeb"
   }
 ], {
@@ -26,6 +26,6 @@ workbox.precaching.precacheAndRoute([
 workbox.clientsClaim()
 workbox.skipWaiting()
 
-workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/holocard/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
-workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/holocard/.*'), workbox.strategies.networkFirst({}), 'GET')
